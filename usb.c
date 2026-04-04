d80af442f2a6e8ce410ac4fc361a9244f72d846e
Johan Hovold <johan@kernel.org>
2026-04-04 11:01:40 +0800

    wifi: rtw89: drop redundant device reference
    Driver core holds a reference to the USB interface and its parent USB
    device while the interface is bound to a driver and there is no need to
    take additional references unless the structures are needed after
    disconnect.
    
    Drop the redundant device reference to reduce cargo culting, make it
    easier to spot drivers where an extra reference is needed, and reduce
    the risk of memory leaks when drivers fail to release it.
    
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20260306093206.21081-1-johan@kernel.org

--- usb.c
+++ usb.c
@@ -989,7 +989,7 @@ static int rtw89_usb_intf_init(struct rtw89_dev *rtwdev,
 	if (!rtwusb->vendor_req_buf)
 		return -ENOMEM;
 
-	rtwusb->udev = usb_get_dev(interface_to_usbdev(intf));
+	rtwusb->udev = interface_to_usbdev(intf);
 
 	usb_set_intfdata(intf, rtwdev->hw);
 
@@ -1003,7 +1003,6 @@ static void rtw89_usb_intf_deinit(struct rtw89_dev *rtwdev,
 {
 	struct rtw89_usb *rtwusb = rtw89_usb_priv(rtwdev);
 
-	usb_put_dev(rtwusb->udev);
 	kfree(rtwusb->vendor_req_buf);
 	usb_set_intfdata(intf, NULL);
 }
